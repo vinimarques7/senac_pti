a7eeb4ee440e802e998902f15f9cb5fee82557ed
Vinícius Marques <viniciusboz1000@outlook.com>
2023-09-04 22:57:34 -0300

    The work for submition is done but still too much work to do

---
+++ script.js
@@ -0,0 +1,16 @@
+// // Referências aos elementos HTML do modal
+// const openLinksModalButton = document.getElementById("openLinksModalButton");
+// const linksModal = document.getElementById("linksModal");
+
+// // Abrir o modal de links quando o botão "Links" é clicado
+// openLinksModalButton.addEventListener("click", () => {
+//     linksModal.style.display = "block";
+// });
+
+// // Ouvinte de evento de clique no documento para fechar o modal
+// document.addEventListener("click", (event) => {
+//     // Verificar se o clique ocorreu fora do modal
+//     if (event.target === linksModal) {
+//         linksModal.style.display = "none";
+//     }
+// });
